db249ba7ccd974c8f1128d8bbe825d60634a7c42
Jonathan Choi <jonathanchoi@v1020-wn-173-131.campus-dynamic.uwaterloo.ca>
2015-09-19 18:58:30 -0400

    inventory toggle

--- js/agency.js
+++ js/agency.js
@@ -24,3 +24,22 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+$(document).ready(function() {
+    // Image swap on hover
+    $("#gallery li img").hover(function(){
+        $('#main-img').attr('src',$(this).attr('src').replace('thumb/', ''));
+    });
+    // Image preload
+    var imgSwap = [];
+     $("#gallery li img").each(function(){
+        imgUrl = this.src.replace('thumb/', '');
+        imgSwap.push(imgUrl);
+    });
+    $(imgSwap).preload();
+});
+$.fn.preload = function() {
+    this.each(function(){
+        $('<img/>')[0].src = this;
+    });
+}
